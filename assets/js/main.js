387abeb5345f0aac82054d4f1ede4e609faabb0f
1omarfaruq <1omarfaruq@gmail.com>
2024-11-14 20:19:56 +0600

    Complete light and dark theme functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
 // Toggle Button
 
-// Dark Light Theme
+
+
+/**
+ * Begin Dark & Light Theme functionality
+ */
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+// This condition will run by default and set mode as light
+if (localStorage.getItem("mode") == "dark") {
+    darkMode();
+} else {
+    lightMode();
+}
+
+themeBtn.addEventListener("click", (event) => {
+    if (localStorage.getItem("mode") == "light") {
+        darkMode();
+    } else {
+        lightMode();
+    }
+});
+
+function darkMode() {
+    html.classList.add("dark");
+    themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+    localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+    html.classList.remove("dark");
+    themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+    localStorage.setItem("mode", "light");
+}
+//End Dark & Light Theme functionality
+
+
 
 // Tabs
 
